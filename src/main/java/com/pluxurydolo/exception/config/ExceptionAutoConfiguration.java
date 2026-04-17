cda9e5ed4787a7ba8ab1e11ae69e907fa7d66cf7
Гвоздев Александр <aleksandr.gvozdev@gazprombank.ru>
2026-04-17 21:15:04 +0300

    17.04.2026

--- src/main/java/com/pluxurydolo/exception/config/ExceptionAutoConfiguration.java
+++ src/main/java/com/pluxurydolo/exception/config/ExceptionAutoConfiguration.java
@@ -3,17 +3,20 @@ package com.pluxurydolo.exception.config;
 import com.pluxurydolo.exception.handler.DefaultExceptionHandler;
 import com.pluxurydolo.exception.util.FileCreator;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 
 @AutoConfiguration
 public class ExceptionAutoConfiguration {
 
     @Bean
+    @ConditionalOnMissingBean
     public DefaultExceptionHandler defaultExceptionHandler(FileCreator fileCreator) {
         return new DefaultExceptionHandler(fileCreator);
     }
 
     @Bean
+    @ConditionalOnMissingBean
     public FileCreator fileCreator() {
         return new FileCreator();
     }
